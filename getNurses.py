fa9876e36d08ae9980c69f0d4090c25ea2e40f6b
martin-wachira <martinnwachira@gmail.com>
2021-03-17 17:50:01 +0300

    loop through all paginations

--- getNurses.py
+++ getNurses.py
@@ -5,6 +5,11 @@ import pandas as pd
 import requests
 import re
 from datetime import datetime
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.firefox.options import Options
+
+options = Options()
+options.add_argument('-headless')
 
 br = webdriver.Firefox()
 url = "https://osp.nckenya.com/LicenseStatus"
@@ -32,27 +37,36 @@ bodyRows_ = body_[1:]
 
 all_rows = []
 
-# loop through all table rows to get all table datas
-sleep(3)
-for row_num in range(len(bodyRows_)):
-    row = []
-    for row_item in bodyRows_[row_num].find_all('td'):
-        stripA = re.sub("(\xa0)|(\n)|,","",row_item.text)
-        row.append(stripA)
+pagNext = br.find_element_by_id('datatable2_next')
 
-    all_rows.append(row)
-    # match each record to its field name
-    cols = ['Nurse Name', 'Licence No.', 'Status', 'View Details']
-    df = pd.DataFrame(data=all_rows, columns=cols)
+# while not (br.find_element_by_class_name('paginate_button_disabled')):
+while True:
+    try:
+        pagNext.click()
+        # loop through all table rows to get all table datas
+        sleep(4)
+        for row_num in range(len(bodyRows_)):
+            row = []
+            for row_item in bodyRows_[row_num].find_all('td'):
+                stripA = re.sub("(\xa0)|(\n)|,","",row_item.text)
+                row.append(stripA)
+                # num_of_pages = br.find_element_by_xpath("//*[@id='datatable2_next']/preceding-sibling::a[1]")
+                # print(len(num_of_pages))
+            all_rows.append(row)
+            # match each record to its field name
+            cols = ['Nurse Name', 'Licence No.', 'Status', 'View Details']
+            # cols = ['name']
+            df = pd.DataFrame(data=all_rows, columns=cols)
+        # print data
+        df.head()
+        print(df.head())
 
-# print data
-df.head()
-# print(df.head())
+        # save nurses list to csv
+        time_now = datetime.now().strftime("%H:%M:%S")
+        time_now = time_now.replace(':','-')
+        time_now = time_now.replace(' ','_')
+        df.to_csv("nurses "+time_now+".csv", index=False)
+        br.quit()
 
-# save nurses list to csv
-time_now = datetime.now().strftime("%H:%M:%S")
-time_now = time_now.replace(':','-')
-time_now = time_now.replace(' ','_')
-df.to_csv("nurses "+time_now+".csv", index=False)
-
-br.quit()
+    except NoSuchElementException:
+        break
