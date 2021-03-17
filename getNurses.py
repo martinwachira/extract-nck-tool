333ffd91d3ef41e06ae28a85bf3827d11789795a
martin-wachira <martinnwachira@gmail.com>
2021-03-17 11:07:24 +0300

    resolve errors

--- getNurses.py
+++ getNurses.py
@@ -4,59 +4,53 @@ from time import sleep
 import pandas as pd
 import requests
 import re
+from datetime import datetime
 
 br = webdriver.Firefox()
 url = "https://osp.nckenya.com/LicenseStatus"
 br.get(url)
-content = br.page_source
-soup = BeautifulSoup(content, 'lxml')
 sleep(2)
 sName = br.find_element_by_xpath("/html/body/div[1]/div[2]/section/div[2]/div/div/div/div/div/div/div[2]/form/div[1]/div/div/input")
 sleep(3)
 sName.send_keys("martin")
 br.find_element_by_xpath("//*[@id='provider']/div[1]/div/div/div/button").click()
-sleep(3)
+sleep(15)
 
-table = soup.find('table')
+content = br.page_source
+soup = BeautifulSoup(content, 'lxml')
 
-body = thead.find_all('tr')
-#
-# get column heads
-head = body[0]
-body_rows = body[1:]
-headings = []
+oTab = soup.find('table')
+print('length of tables ', len(oTab))
 
-for item in head.find_all('th'):
-    item = (item.text).rstrip("\n")
-    headings.append(item)
+table_ = oTab
+body_ = table_.find_all('tr')
+print('length of table records. ', len(body_))
+heads = body_[0]
+th = heads.find_all('th')
 
-print(headings)
-#declare an empty list for holding all records
+bodyRows_ = body_[1:]
+
 all_rows = []
 
 # loop through all table rows to get all table datas
-
-# for nurses in table.find_all('tbody'):
-#     rows = nurses.find_all('tr')
-#     for row in rows:
-#         nurseRecords = row.find('td').text
-#         sleep(2)
-#         print(nurseRecords)
-
-for row_num in range(len(body_rows)):
+sleep(3)
+for row_num in range(len(bodyRows_)):
     row = []
-    for row_item in body_rows[row_num].find_all('td'):
+    for row_item in bodyRows_[row_num].find_all('td'):
         stripA = re.sub("(\xa0)|(\n)|,","",row_item.text)
         row.append(stripA)
 
-all_rows.append(row)
-
-# match each record to its field name
-# cols = ['name', 'license', 'xxx', 'xxxx']
-df = pd.DataFrame(data=all_rows, columns=headings)
+    all_rows.append(row)
+    # match each record to its field name
+    cols = ['Nurse Name', 'Licence No.', 'Status', 'View Details']
+    df = pd.DataFrame(data=all_rows, columns=cols)
 
 # print data
 df.head()
+# print(df.head())
 
 # save nurses list to csv
-df.to_csv('nurses.csv')
+time_now = datetime.now().strftime("%H:%M:%S")
+time_now = time_now.replace(':','-')
+time_now = time_now.replace(' ','_')
+df.to_csv("nurses "+time_now+".csv", index=False)
