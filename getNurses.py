ca587187267ff0590c4e08f217a6378557e1eff9
martin-wachira <martinnwachira@gmail.com>
2021-03-17 18:39:24 +0300

    separate try/catch block from for loop

--- getNurses.py
+++ getNurses.py
@@ -37,36 +37,35 @@ bodyRows_ = body_[1:]
 
 all_rows = []
 
-pagNext = br.find_element_by_id('datatable2_next')
-
 # while not (br.find_element_by_class_name('paginate_button_disabled')):
 while True:
-    try:
-        pagNext.click()
-        # loop through all table rows to get all table datas
-        sleep(4)
-        for row_num in range(len(bodyRows_)):
-            row = []
-            for row_item in bodyRows_[row_num].find_all('td'):
-                stripA = re.sub("(\xa0)|(\n)|,","",row_item.text)
-                row.append(stripA)
-                # num_of_pages = br.find_element_by_xpath("//*[@id='datatable2_next']/preceding-sibling::a[1]")
-                # print(len(num_of_pages))
-            all_rows.append(row)
-            # match each record to its field name
-            cols = ['Nurse Name', 'Licence No.', 'Status', 'View Details']
-            # cols = ['name']
-            df = pd.DataFrame(data=all_rows, columns=cols)
-        # print data
-        df.head()
-        print(df.head())
+    # loop through all table rows to get all table datas
+    sleep(3)
+    for row_num in range(len(bodyRows_)):
+        row = []
+        for row_item in bodyRows_[row_num].find_all('td'):
+            stripA = re.sub("(\xa0)|(\n)|,","",row_item.text)
+            row.append(stripA)
+        all_rows.append(row)
+        # match each record to its field name
+        cols = ['Nurse Name', 'Licence No.', 'Status', 'View Details']
+        # cols = ['name']
+        df = pd.DataFrame(data=all_rows, columns=cols)
 
-        # save nurses list to csv
-        time_now = datetime.now().strftime("%H:%M:%S")
-        time_now = time_now.replace(':','-')
-        time_now = time_now.replace(' ','_')
-        df.to_csv("nurses "+time_now+".csv", index=False)
-        br.quit()
+    try:
+        pagNext = br.find_element_by_id('datatable2_next')
+        pagNext.click()
 
     except NoSuchElementException:
         break
+
+    # print data
+    df.head()
+    print(df.head())
+
+    # save nurses list to csv
+    time_now = datetime.now().strftime("%H:%M:%S")
+    time_now = time_now.replace(':','-')
+    time_now = time_now.replace(' ','_')
+    df.to_csv("nurses "+time_now+".csv", index=False)
+    # br.quit()
